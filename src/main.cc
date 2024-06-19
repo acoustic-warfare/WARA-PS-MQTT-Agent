486ac4da1b479d89968a1964828c1fa82a4838d7
Janne Schyffert <janne.schyffert@me.com>
2024-06-19 14:11:29 +0200

    Allow setting callback methods for different topics and commands

--- src/main.cc
+++ src/main.cc
@@ -2,15 +2,18 @@
 #include "waraps_client.h"
 #include <unistd.h>
 
-int main()
-{
+int main() {
     waraps_client client("test", "mqtt://localhost:25565");
     std::cout << "Client created" << std::endl;
-    client.start();
+    std::thread client_thread = client.start();
 
-    while(client.running()) {
-        sleep(5);
-    }
+    auto f = [&](const nlohmann::json &_) {
+        client.publish_message("exec/response", std::string("AAAAAAAAAAA"));
+    };
+
+    client.set_command_callback("scream", f);
+
+    client_thread.join();
 
     return 0;
 }
--- src/waraps_client.h
+++ src/waraps_client.h
@@ -13,18 +13,22 @@
  *
  * Author: Janne Schyffert
  */
-class waraps_client
-{
+class waraps_client {
 private:
     const std::chrono::milliseconds heartbeat_interval =
-        std::chrono::milliseconds(DEFAULT_HEARTBEAT_INTERVAL);
+            std::chrono::milliseconds(DEFAULT_HEARTBEAT_INTERVAL);
     const std::string UNIT_NAME, SERVER_ADDRESS;
 
     std::string static generate_uuid();
-    static std::string generate_full_topic(const std::string& topic) ;
+
+    static std::string generate_full_topic(const std::string &topic);
+
     std::string generate_heartbeat_message() const;
-    bool handle_message(const mqtt::const_message_ptr& msg);
+
+    void handle_message(const mqtt::const_message_ptr &msg);
+
     void cmd_pong(nlohmann::json msg_payload);
+
     void cmd_stop(nlohmann::json msg_payload);
 
     // Commands get a separate function and map to allow different command callbacks as well as user-defined command callbacks
@@ -35,10 +39,10 @@ private:
     mqtt::async_client client;
     std::shared_ptr<bool> is_running = std::make_shared<bool>(false);
     std::map<std::string, std::function<void(waraps_client *, nlohmann::json)>> message_callbacks{
-        {"exec/command", &waraps_client::handle_command}};
+            {"exec/command", &waraps_client::handle_command}};
     std::map<std::string, std::function<void(waraps_client *, nlohmann::json)>> command_callbacks{
-        {"stop", &waraps_client::cmd_stop},
-        {"pong", &waraps_client::cmd_pong}};
+            {"stop", &waraps_client::cmd_stop},
+            {"ping", &waraps_client::cmd_pong}};
 
 public:
     waraps_client(std::string name, std::string server_address);
@@ -47,8 +51,11 @@ public:
      * Deleted constructors to disallow copying and moving of the client
      */
     waraps_client(waraps_client &&) = delete;
+
     waraps_client(const waraps_client &) = delete;
+
     waraps_client &operator=(const waraps_client &) = delete;
+
     waraps_client &operator=(waraps_client &&) = delete;
 
     ~waraps_client();
@@ -63,7 +70,7 @@ public:
      * Start main thread and heartbeat thread and begin consuming messages from the MQTT server.
      * Will not block the current thread.
      */
-    void start();
+    std::thread start();
 
     /**
      * Stop consuming messages and disable the heartbeat thread, allowing the client to be destroyed.
@@ -75,25 +82,33 @@ public:
      * @param topic The topic to publish the message to, will be added to WARA PS topic prefix
      * @param payload The message to publish as a JSON string
      */
-    bool publish_message_async(const std::string& topic, const std::string& payload);
+    void publish_message(const std::string &topic, const std::string &payload);
 
     /**
-     * Set an asyncronous callback function to be called when a message is recieved on the specified topic.
+     * Set an asynchronous callback function to be called when a message is received on the specified topic.
      * Disallows setting the "command" topic callback, see set_command_callback for that.
      * @param topic the topic to listen on, will be added to WARA PS topic prefix.
-     * @param callback the function to call when a message is recieved on the specified topic, takes waraps_client as a parameter to allow for state changes if needed.
+     * @param callback the function to call when a message is received on the specified topic, takes waraps_client as a parameter to allow for state changes if needed.
      * @throws std::invalid_argument if the topic is "exec/command"
      */
-    void set_message_callback(const std::string& topic, std::function<void(waraps_client *, nlohmann::json)> callback);
+    void set_message_callback(const std::string &topic, std::function<void(waraps_client *, nlohmann::json)> callback);
 
     /**
-     * Set an asyncronous callback function to be called when a message is recieved on the specified topic.
+     * Set an asynchronous callback function to be called when a message is received on the specified topic.
      * Disallows setting the "command" topic callback, see set_command_callback for that.
      * @param topic the topic to listen on, will be added to WARA PS topic prefix.
-     * @param callback the function to call when a message is recieved on the specified topic
+     * @param callback the function to call when a message is received on the specified topic
      * @throws std::invalid_argument if the topic is "exec/command"
      */
-    void set_message_callback(std::string topic, std::function<void(nlohmann::json)> callback);
+    void set_message_callback(const std::string &topic, const std::function<void(nlohmann::json)> &callback);
+
+    /**
+     * Set an asynchronous callback function to be called when a command is received on the /exec/command topic
+     * @param command The type of command to call the callback with
+     * @param callback The callback to use
+     * @throws std::invalid_argument if the reserved commands "ping" or "stop" are given
+     */
+    void set_command_callback(const std::string &command, const std::function<void(nlohmann::json)> &callback);
 };
 
 #endif
